2edbea2cdf62bb046ec20e76614413dd9f9e9c4b
Gracie Taylor <53783124+taylogra@users.noreply.github.com>
2019-12-19 09:41:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let Start=function(){
+
+	let content= "<p>ahhhhh</p><br><p>You hear a loud scream coming from somewhere behind you. You quickly stand and realize you aren't in your bedroom anymore, you're standing in the middle of the woods. A young boy sits in front of you staring with a confused look on his face. He is also unsure how he got there.</p><br>"
+
+	content+="<p>As you looked around you noticed 2 paths; 1 leading into a cave and the other leading to a cabin</p><input type= 'radio' name='' id='radio1'<label for='radio1'>Cabin</label><input "
+	
+	document.getElementById("Start").innerHTML=content;
+
+let radio1 = document.getElementById = ("Cabin");
+let radio2 = document.getElementById = ("Cave");
+}
+
+
+
